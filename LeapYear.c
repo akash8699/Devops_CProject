c5c1de02de691bec5ddfd9c9446171f67f2a2506
Akash_Anantwal <73925309+akash8699@users.noreply.github.com>
2025-05-08 18:09:30 +0530

    Create LeapYear.c

---
+++ LeapYear.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s <year>\n", argv[0]);
+        return 1;
+    }
+
+    int year = atoi(argv[1]);
+
+    if (year <= 0) {
+        printf("Invalid year. Please enter a positive integer.\n");
+        return 1;
+    }
+
+    if ((year % 400 == 0) || (year % 100 != 0 && year % 4 == 0)) {
+        printf("%d is a Leap Year.\n", year);
+    } else {
+        printf("%d is not a Leap Year.\n", year);
+    }
+
+    return 0;
+}
