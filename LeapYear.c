8734e76b168f06c6b387382b4b9d15b3df163262
Akash_Anantwal <73925309+akash8699@users.noreply.github.com>
2025-05-08 18:19:03 +0530

    Update LeapYear.c

--- LeapYear.c
+++ LeapYear.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+//This program is to find leap yaer
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         printf("Usage: %s <year>\n", argv[0]);
